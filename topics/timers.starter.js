a52c0baba17f2feef0dc41aabff419abbb9872c9
r1048035 <r1048035@student.thomasmore.be>
2025-03-04 10:41:13 +0100

    timers

--- topics/timers.starter.js
+++ topics/timers.starter.js
@@ -8,41 +8,38 @@ _/  |_|__| _____   ___________  ______
 */
 
 // 1 — start a timer and show a ⏰ in #clock after 10 seconds
-let clockTimeout = setTimeout(() => {
-  document.getElementById('clock').innerText = '⏰';
-}, 10000);
+let seconds = 0;
+let timer = setInterval(() => {
+  seconds++;
+  document.getElementById("clock").innerHTML = seconds;
+  if (seconds == 10) {
+    clearInterval(timer);
+    document.getElementById("clock").innerHTML = "⏰";
+  }
+}, 1000);
 
 // 2 — click the start button to show a ticking clock in #timer that goes up every second
 // clicking stop should clear and stop the timer and reset the clock to 0
-let timerInterval;
-function startTimer() {
-  let counter = 0;
-  document.getElementById('timer').innerText = counter;
-  timerInterval = setInterval(() => {
-      counter++;
-      document.getElementById('timer').innerText = counter;
+document.getElementById("start").addEventListener("click", () => {
+  let seconds = 0;
+  let timer = setInterval(() => {
+    seconds++;
+    document.getElementById("timer").innerHTML = seconds;
   }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(timerInterval);
-  document.getElementById('timer').innerText = 0;
-}
-
-// Image Slideshow
+  document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(timer);
+    document.getElementById("timer").innerHTML = 0;
+  });
+});
 
 // 3 — create an image gallery that shows a new image every 5 seconds and repeats that cycle
 // use the images gallery1.jpg, gallery2.jpg, gallery3.jpg in the images/timers_intervals folder
 // use only one img tag in the html and change its src attribute (#slideshow)
-const images = [
-  'images/timers_intervals/gallery1.jpg',
-  'images/timers_intervals/gallery2.jpg',
-  'images/timers_intervals/gallery3.jpg'
-];
-let currentImageIndex = 0;
-function changeImage() {
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  document.getElementById('slideshow').src = images[currentImageIndex];
-}
-setInterval(changeImage, 5000);
-
+let images = ["gallery1.jpg", "gallery2.jpg", "gallery3.jpg"];
+let i = 0;
+setInterval(() => {
+  document.getElementById(
+    "slideshow"
+  ).src = `../images/timers_intervals/${images[i]}`;
+  i = (i + 1) % images.length;
+}, 5000);
